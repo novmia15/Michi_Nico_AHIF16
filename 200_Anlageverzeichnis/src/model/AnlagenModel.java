d1af44ffa3ae9f0346004be6291e0efd7214b6e0
novmia15 <novmia15@htlkaindorf.at>
2018-11-13 15:54:44 +0100

    update

---
+++ 200_Anlageverzeichnis/src/model/AnlagenModel.java
@@ -0,0 +1,37 @@
+package model;
+
+import data.Anlage;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class AnlagenModel extends AbstractTableModel
+{
+	private List<Anlage> anlagen = new ArrayList<Anlage>();
+
+	@Override
+	public int getRowCount()
+	{
+		
+	}
+
+	@Override
+	public int getColumnCount()
+	{
+		
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		
+	}
+
+	@Override
+	public String getColumnName(int column)
+	{
+		
+	}
+	
+	
+}
