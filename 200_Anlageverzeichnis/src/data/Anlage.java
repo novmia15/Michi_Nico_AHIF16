1c6170109ad3e21475e234dbb379fcc170546abc
Nico Plattner <plania16@htlkaindorf.at>
2018-11-15 14:10:46 +0100

    code korrektur

--- 200_Anlageverzeichnis/src/data/Anlage.java
+++ 200_Anlageverzeichnis/src/data/Anlage.java
@@ -1,6 +1,5 @@
 package data;
 
-//import java.time.LocalDate;
 public class Anlage {
 
     private final int kaufPreis;
@@ -26,27 +25,22 @@ public class Anlage {
     }
 
     public double getBisherigeNutzdauer() {
-        //System.out.println("Bisher. Nutzdauer: "+((nutzungsdauer+inbetriebnahme)-2018));
         return (nutzungsdauer + inbetriebnahme) - 2018;
     }
 
     public double getAfabisher() {
-        //System.out.println("Afa bisher: "+((kaufPreis/nutzungsdauer)/((nutzungsdauer+inbetriebnahme)-2018)));
         return (kaufPreis / nutzungsdauer) / ((nutzungsdauer + inbetriebnahme) - 2018);
     }
 
     public double getWertvorAfa() {
-        //System.out.println("Wert vor Afa: "+(kaufPreis-(kaufPreis/nutzungsdauer)/((nutzungsdauer+inbetriebnahme)-2018)));
         return kaufPreis - (kaufPreis / nutzungsdauer) / ((nutzungsdauer + inbetriebnahme) - 2018);
     }
 
     public double getAfadiesesJahres() {
-        //System.out.println("AfadJ: "+(kaufPreis/nutzungsdauer));
         return kaufPreis / nutzungsdauer;
     }
 
     public double getBuchwert() {
-        //System.out.println("Buchwert: "+(kaufPreis-(kaufPreis/nutzungsdauer)/((nutzungsdauer+inbetriebnahme)-2018)-(kaufPreis/nutzungsdauer)));
         return kaufPreis - (kaufPreis / nutzungsdauer) / ((nutzungsdauer + inbetriebnahme) - 2018) - (kaufPreis / nutzungsdauer);
     }
 
